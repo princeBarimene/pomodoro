a091c37b2bb7dca6ea98f5c06f911b2139db3e54
princeBarimene <prince.g.olubari@gmail.com>
2024-11-22 15:38:10 +0000

    added a text and adjusted the image

--- main.py
+++ main.py
@@ -23,12 +23,13 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("Pomodoro App")
-window.config(padx=100,pady=50)
+window.config(padx=100,pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
+canvas = Canvas(width=200, height=224, bg = YELLOW, highlightthickness=0)
 
 tomato_img = PhotoImage(file="./tomato.png")
-canvas.create_image(101,112,image = tomato_img)
+canvas.create_image(100,112,image = tomato_img)
+canvas.create_text(100,135, text="00:00", fill = "white", font = (FONT_NAME,25, "bold"))
 canvas.pack()
 
 
