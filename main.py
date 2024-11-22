46848fde03a4c4b206dabaa9bea93e47e8527cc5
princeBarimene <prince.g.olubari@gmail.com>
2024-11-22 15:24:07 +0000

    added an image to the canvas

--- main.py
+++ main.py
@@ -23,8 +23,13 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("Pomodoro App")
+window.config(padx=100,pady=50)
 
+canvas = Canvas(width=200, height=224)
 
+tomato_img = PhotoImage(file="./tomato.png")
+canvas.create_image(101,112,image = tomato_img)
+canvas.pack()
 
 
 
