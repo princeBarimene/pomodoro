acc4f08c0cd19885cdc11e8adf6882e73e7c186f
princeBarimene <prince.g.olubari@gmail.com>
2024-11-23 01:25:08 +0000

    completed the pomodoro app for work timings

--- main.py
+++ main.py
@@ -13,23 +13,70 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
+def reset_timer():
+    window.after_cancel(timer)
+    title_label.config(text="Timer")
+    canvas.itemconfig(timer_text, text="00:00")
+    check_mark_label.config(text="")
+    global reps
+    reps = 0
+
+
+
+
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+
+def start_timer():
+
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec =LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        countdown(long_break_sec)
+        title_label.config(text="Break", font=(FONT_NAME,25,"bold"),fg=RED)
+
+    elif reps % 2 == 0:
+        countdown(short_break_sec)
+        title_label.config(text="Break", font=(FONT_NAME,25,"bold"),fg=PINK)
+    
+    else:
+
+        countdown(work_sec)
+        title_label.config(text="Work", font=(FONT_NAME,25,"bold"),fg=GREEN)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 def countdown(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000,countdown,count-1)
+        global timer
+        timer = window.after(1000,countdown,count-1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps / 2)
+        for _ in range(work_sessions):
+            marks += "✓"
+        check_mark_label.config(text=marks)
 
 
-def start_timer():
-    countdown(5 * 60)
+
 
 
 
@@ -62,10 +109,10 @@ title_label.grid(column=1, row=0)
 start_button = Button(text="Start", highlightthickness=0, bg="white", command=start_timer)
 start_button.grid(column= 0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0, bg="white" )
+reset_button = Button(text="Reset", highlightthickness=0, bg="white", command=reset_timer)
 reset_button.grid(column= 2, row=2)
 
-check_mark_label = Label(text="✓", fg = GREEN, font=(FONT_NAME,35,"bold"), bg=YELLOW)
+check_mark_label = Label(fg = GREEN, font=(FONT_NAME,35,"bold"), bg=YELLOW)
 check_mark_label.grid(column=1, row=3)
 
 
